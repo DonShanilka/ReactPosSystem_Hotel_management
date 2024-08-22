7dc8d99d554d1b1d7edbfb10b901a86e14fe031e
Shanilka800@# <shanilka799@gmail.com>
2024-08-22 12:04:19 +0530

    add dependency, and slide bar page components

---
+++ src/components/Sidebar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { List, ListItem, ListItemText, Drawer } from '@mui/material';
+
+const Sidebar = () => {
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{ width: 240, flexShrink: 0, '& .MuiDrawer-paper': { width: 240, boxSizing: 'border-box' } }}
+    >
+      <List>
+        <ListItem button component={Link} to="/">
+          <ListItemText primary="Home" />
+        </ListItem>
+        <ListItem button component={Link} to="/customers">
+          <ListItemText primary="Customers" />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
