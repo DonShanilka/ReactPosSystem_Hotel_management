8e64cdec73a8ccd5ff506cfece07d715706cb570
Shanilka800@# <shanilka799@gmail.com>
2024-09-09 16:04:15 +0530

    update

--- src/pages/CustomerAddPage.jsx
+++ src/pages/CustomerAddPage.jsx
@@ -1,57 +1,26 @@
-import React, { useState } from 'react';
+
+import * as React from 'react';
 import './CustomerPage.css'
 import SearchAppBar from '../components/SerchBar';
 import Table from '../components/TableComponent';
-import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Model from 'react-modal'
-import zIndex from '@mui/material/styles/zIndex';
 import '../pages/CustomerPage.css'
-import Form from '../components/Forms';
 import TextField from '@mui/material/TextField';
-import { Margin, Padding } from '@mui/icons-material';
-import MenuItem from '@mui/material/MenuItem';
+
+
+
 
 const CustomerAddPage = () => {
-  const [customer, setCustomer] = useState({ name: '', email: '' });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setCustomer({ ...customer, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission, e.g., send data to server
-    console.log('Customer added:', customer);
-  };
 
   const [visible, setVisible] = useState(false)
 
-  const currencies = [
-    {
-      value: 'USD',
-      label: '$',
-    },
-    {
-      value: 'EUR',
-      label: '€',
-    },
-    {
-      value: 'BTC',
-      label: '฿',
-    },
-    {
-      value: 'JPY',
-      label: '¥',
-    },
-  ];
 
   return (
     <>
         <div className='customerPageMainDiv'>
             <SearchAppBar/>
-            <Button variant="contained" color="success" sx={{position:"absolute", right:"2%", top:"20%"}} onClick={() => setVisible(true)}>+ Add Customer</Button>
+            <Button variant="contained" color="success" sx={{position:"absolute", right:"2%", top:"20%"}} onClick={() => setVisible(true)}>+  Add Customer</Button>
             <Table/>
 
             <Model isOpen={visible} style={{
@@ -75,26 +44,14 @@ const CustomerAddPage = () => {
                 <br /> <br />
                 <TextField sx={{margin: "10px"}} id="outlined-basic" label="ID" variant="outlined" />
                 <TextField sx={{margin: "10px"}} id="outlined-basic" label="Name" variant="outlined" />
-                <TextField sx={{margin: "10px"}} id="outlined-basic" label="Address" variant="outlined" />
+                <TextField sx={{margin: "10px", width:"94%"}} id="outlined-basic" label="Address" variant="outlined" />
+                <TextField sx={{margin: "10px",width:"94%"}} id="outlined-basic" label="Email" variant="outlined" />
                 <TextField sx={{margin: "10px"}} id="outlined-basic" label="Country" variant="outlined" />
-                <TextField sx={{margin: "10px"}} id="outlined-basic" label="Email" variant="outlined" />
                 <TextField sx={{margin: "10px"}} id="outlined-basic" label="Room No:" variant="outlined" />
-                <TextField sx={{margin: "10px"}} id="outlined-basic" label="Booking Date" variant="outlined" />
+                <TextField sx={{margin: "10px"}} id="outlined-basic" label="Booking Date" variant="outlined" /> 
                 <TextField sx={{margin: "10px"}} id="outlined-basic" label="Out Date" variant="outlined" />
-                <TextField sx={{margin: "10px", width:"91%"}}
-          id="outlined-select-currency"
-          select
-          label="Select"
-          defaultValue="EUR"
-          helperText="Please select your currency"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-                <br /><br /><br /><br />
+                
+                <br /><br /><br />
                 <Button sx={{margin: "10px"}} variant="contained" color="success">Save</Button>
                 <Button variant="contained">Update</Button>
               </div>
