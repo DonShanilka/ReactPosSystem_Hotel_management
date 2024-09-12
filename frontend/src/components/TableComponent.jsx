195efbde8b851dce0b167bb68d46ba454cae5d5f
Shanilka800@# <shanilka799@gmail.com>
2024-09-12 23:19:21 +0530

    add new table

--- frontend/src/components/TableComponent.jsx
+++ frontend/src/components/TableComponent.jsx
@@ -1,28 +1,67 @@
 import * as React from "react";
 import axios from "axios";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+function createData(cid, name, age, address, email) {
+  return { cid, name, age, address, email };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
 
 export default function TableComponent() {
-
-  // const URL = "http://localhost:5001/customers"; 
-
-  // const fettchHandler = async () => {
-  //   return await axios.get(URL).then((res) => res.data);
-  // };
-
-
   // ********************************************************
   const [customer, setCustomer] = useState([]);
-    useEffect(() => {
-      axios.get('http://localhost:5001/customers')
-      .then(customer => setCustomer(customer.data))
-      .catch(err => console.log((err)))
-    }, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5001/customers")
+      .then((customer) => setCustomer(customer.data))
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-      <>
-        
-      </>
-    );
-  }
-
+  return (
+    <>
+      <TableContainer component={Paper} >
+        <Table sx={{position:"absolute" ,top:"30%",left:"17%" ,width:"80%"}} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Dessert (100g serving)</TableCell>
+              <TableCell align="right">Calories</TableCell>
+              <TableCell align="right">Fat&nbsp;(g)</TableCell>
+              <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+              <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+}
