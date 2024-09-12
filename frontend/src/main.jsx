a3e346a5da604b9f7a7a3190268198ac574aacf3
Shanilka800@# <shanilka799@gmail.com>
2024-09-12 23:08:18 +0530

    reamove old table

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,11 @@
-import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
