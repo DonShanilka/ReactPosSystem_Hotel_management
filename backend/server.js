22a52cca4265f226a81f75ce1a21ce88863ef035
Shanilka800@# <shanilka799@gmail.com>
2024-09-13 22:17:45 +0530

    Update servr file

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,19 @@
-//ABqz8GyMZhI70BMz
-
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const cors = require("cors");
 const router = require("./Routes/CustomerRoutes");
 const port = 5001;
-const {MongoClient,ObjectId} = require('mongodb');
-var bodyParser = require('body-parser');
 
 app.use(cors());
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json);
-
-// const uri =
-//   "mongodb+srv://shanilka100:ABqz8GyMZhI70BMz@shanilka.15lsu.mongodb.net/";
-
-const url = 'mongodb://127.0.0.1:27017/testHotel';
-const client = new MongoClient(url);
-
-const dbname ='testHotel';
-
-const db = client.db(dbname);
-const collection = db.collection('customer');
+const uri =
+  "mongodb+srv://shanilka100:ABqz8GyMZhI70BMz@shanilka.15lsu.mongodb.net/";
 
 const connect = async () => {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(uri);
     console.log("Connect to MongoDB");
   } catch (error) {
     console.log('MongoDB Error', error);         
